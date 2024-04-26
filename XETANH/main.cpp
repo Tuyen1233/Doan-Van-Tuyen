cc0bee8ac23c0cba0a3aec72cdb71911dd8b7d18
Tuyen1233 <vant47548@gmail.com>
2024-04-26 13:03:34 +0700

    hoan thanh

--- XETANH/main.cpp
+++ XETANH/main.cpp
@@ -13,15 +13,7 @@ int main(int argc, char* argv[])
 
 	while (kk == 1) {
 	
-		Mix_Init(MIX_INIT_MP3);
-		Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
-
-		// Load âm thanh MP3 vào biến Mix_Music
-		Mix_Music* music = Mix_LoadMUS("nhacnen.mp3");
-
-		// Phát âm thanh MP3
-		Mix_PlayMusic(music, 1);
-
+		
  	game = new Game();
 
 	game->init("GameWindow", 800, 640, false);
